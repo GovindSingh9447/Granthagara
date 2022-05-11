16e70d3b107690ba21a6122e557fff5cd8cda39c
GovindSingh9447 <govindkrsingh0605@gmail.com>
2022-05-12 00:18:01 +0530

    Adding Notification features

--- app/src/main/java/com/ranawat/Notifications/FirebaseMessagingService.java
+++ app/src/main/java/com/ranawat/Notifications/FirebaseMessagingService.java
@@ -1,28 +1,26 @@
 package com.ranawat.Notifications;
 
-import android.annotation.SuppressLint;
 import android.app.Notification;
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
-import android.app.Service;
+
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.media.Ringtone;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Build;
-import android.os.IBinder;
 import android.os.Vibrator;
-import android.util.Log;
-import android.widget.Toast;
+
+
 
 import androidx.core.app.NotificationCompat;
-import androidx.core.app.NotificationManagerCompat;
+
 
 
 import com.google.firebase.messaging.RemoteMessage;
+import com.ranawat.collagenotes.DashboardUserActivity;
 
 public class FirebaseMessagingService extends com.google.firebase.messaging.FirebaseMessagingService {
 
@@ -63,7 +61,7 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
 
 
-        Intent resultIntent = new Intent(this, SplashScreen.class);
+        Intent resultIntent = new Intent(this, DashboardUserActivity.class);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 1, resultIntent, PendingIntent.FLAG_UPDATE_CURRENT);
 
 
@@ -76,9 +74,6 @@ public class FirebaseMessagingService extends com.google.firebase.messaging.Fire
 
         mNotificationManager =
                 (NotificationManager) getApplicationContext().getSystemService(Context.NOTIFICATION_SERVICE);
-
-
-
 
 
 
